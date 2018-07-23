d01e5b397c0c0be5170e79c430554bc0e9632cce
Demo User <demouser@MacBook-Pro.local>
2018-07-23 11:41:31 -0400

    Added Google sign in

--- main.py
+++ main.py
@@ -11,18 +11,27 @@ jinja_current_directory = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_current_directory.get_template('templates/skeleton.html')
-        self.response.write(template.render())
-
         user = users.get_current_user()
+
+        #assign these to something so the python runs no matter what
+        nickname = None
+        logout_url = None
+        login_url = None
+
         if user:
             nickname = user.nickname()
             logout_url = users.create_logout_url('/')
-            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
-                nickname, logout_url)
         else:
             login_url = users.create_login_url('/')
-            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+
+        template_vars = {
+            "user": user,
+            "nickname": nickname,
+            "logout_url": logout_url,
+            "login_url": login_url,
+        }
+        template = jinja_current_directory.get_template('templates/skeleton.html')
+        self.response.write(template.render(template_vars))
 
 
 app = webapp2.WSGIApplication([
